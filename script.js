c755fdde27871767355424964388ac0d00a61d75
aathi-aathi <aathivdr2004@gmail.com>
2024-08-13 15:38:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Sample data array
+
+const getData = async () => {
+  const response = await fetch(`https://cat-fact.herokuapp.com/facts`,{
+    method:'GET'
+  })
+ const data = await response.json()
+ console.log(data)
+ renderTable(data)
+}
+// Function to render data into the table
+ async function renderTable(data) {
+    const dataList = document.getElementById('dataList');
+    data.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item._id}</td>
+            <td>${item.user}</td>
+            <td>${item.text}</td>
+        `;
+        dataList.appendChild(row);
+    });
+}
+getData()
+// Call the function to render data
